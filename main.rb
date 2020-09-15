68a7bae4022b119a6018fc690aa874b45a130b7f
NtwaliHeritier <heritierntwali@gmail.com>
2020-09-15 11:30:15 +0200

    completed myeach function

--- main.rb
+++ main.rb
@@ -9,4 +9,15 @@ module Enumerable
       i += 1
     end
   end
+  # my_each_with_index method
+  def my_each_with_index
+    return to_enum unless block_given?
+
+    i = 0
+    while i < to_a.length
+      yield(to_a[i], i)
+      i += 1
+    end
+  end
+
 end
