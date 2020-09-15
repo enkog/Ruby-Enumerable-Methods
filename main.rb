05a62720dbd2ad10db87a2319b5398d736c08ac3
enkog <nkirukaogu@gmail.com>
2020-09-15 10:32:08 +0100

    Add my_select method

--- main.rb
+++ main.rb
@@ -9,7 +9,8 @@ module Enumerable
       i += 1
     end
   end
-  # my_each_with_index method
+
+# my_each_with_index method
   def my_each_with_index
     return to_enum unless block_given?
 
@@ -18,6 +19,15 @@ module Enumerable
       yield(to_a[i], i)
       i += 1
     end
+  end
+
+# my_select method
+  def my_select
+    return to_enum unless block_given?
+
+    arr = []
+    to_a.my_each { |i| arr << i if yield i }
+    arr
   end
 
 end
