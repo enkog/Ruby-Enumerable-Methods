607bdb357835fbf1fa1572112bae36876a5baef8
enkog <nkirukaogu@gmail.com>
2020-09-15 10:25:45 +0100

    Add my_each method

--- main.rb
+++ main.rb
@@ -1,3 +1,12 @@
 module Enumerable
+# my_each method
+  def my_each
+    return to_enum unless block_given?
 
+    i = 0
+    while i < to_a.length
+      yield to_a[i]
+      i += 1
+    end
+  end
 end
