035a55445261f296bb8a783ae070484136507e88
Ceejayski <61045640+Ceejayski@users.noreply.github.com>
2020-10-26 11:37:12 +0100

    Add test for all methods
    Added test for my_count method
    
    Added test for my_map method
    
    Added test for my_inject method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,10 +1,10 @@
 require './lib/enumerable.rb'
 
 RSpec.describe 'Enumerable' do
-  let(:num_arr) { [1, 2, 4, 6, 8, 21] }
+  let(:num_arr) { [1, 2, 4, 6, 8, 21, 2] }
   let(:test_range) { (1..5) }
   let(:test_hash) { { a: 100, c: 300, b: 200 } }
-  let(:test_arr) { %w[hello hi dog] }
+  let(:test_arr) { %w[hello hi dog mum] }
   let(:test_proc) { proc { |num| num * 2 } }
   let(:test_search) { proc { |memo, word| memo.length > word.length ? memo : word } }
   # my_each method
@@ -56,7 +56,7 @@ RSpec.describe 'Enumerable' do
   # select method
   context '#select' do
     it 'returns an enumerator if no block is given' do
-      expect(test_arr.my_each).to be_a(Enumerator)
+      expect(test_arr.my_select).to be_a(Enumerator)
     end
 
     it 'returns all elements of the array for which the block returns a true' do
@@ -175,6 +175,73 @@ RSpec.describe 'Enumerable' do
 
     it 'checks if none element of the range is positive' do
       expect(test_range.my_none?(&:positive?)).to eq(false)
+    end
+  end
+
+  describe "my_count" do
+
+    it "return the back the length of the array if block was not given" do
+      expect(num_arr.my_count).to eq(num_arr.count)
+    end
+
+    it "return the number of occurrences of the argument in the array" do
+      expect(num_arr.my_count(2)).to eq(num_arr.count(2))
+    end
+
+    it "return the number of occurrences of the block condition in the array" do
+      expect(num_arr.my_count{|num| num % 2 == 0}).to eq(num_arr.count{|num| num % 2 == 0})
+    end
+
+    it "return the number of occurrences of the block condition in the array" do
+      expect(test_arr.my_count{|string| string.length == 2}).to eq(test_arr.count{|string| string.length == 2})
+    end
+
+    it "return the number of occurrences of the block condition in the range" do
+      expect(test_range.my_count{|num| num % 2 == 0}).to eq(test_range.count{|num| num % 2 == 0})
+    end
+  end
+
+  describe 'my_map' do
+    it 'returns an enumerator if no block is given' do
+      expect(test_arr.my_map).to be_a(Enumerator)
+    end
+
+    it "returns a transformed array according to the block given" do
+      expect(num_arr.my_map{|i| i*i}).to eq(num_arr.map{|i| i*i})
+    end
+
+    it "returns a transformed array according to the block given to the range" do
+      expect(test_range.my_map{|i| i*i}).to eq(test_range.map{|i| i*i})
+    end
+
+    it "returns a transformed array according to the block given" do
+      expect(test_arr.my_map{|i| i.to_sym}).to eq(test_arr.map{|i| i.to_sym})
+    end
+
+    it "returns a transformed array according to the block given" do
+      expect(test_hash.my_map{|v,i| i.to_s}).to eq(test_hash.map{|v,i| i.to_s})
+    end
+
+    it "accepts proc and lambdas and calls them as a block" do
+      expect(num_arr.my_map(&test_proc)).to eq(num_arr.map(&test_proc))
+    end
+  end
+
+  describe 'my_inject' do
+    it 'returns the Sum of all the element of the array' do
+      expect(num_arr.my_inject(:+)).to eql(num_arr.inject(:+))
+    end
+
+    it 'returns the Sum of all the element of the range' do
+      expect(test_range.my_inject { |total, item| total + item }).to eql(test_range.inject { |total, item| total + item })
+    end
+
+    it ' multiply numbers inside an array or range when an accumulator and a symbol are passed as arguments' do
+      expect((1..10).my_inject(2, :*)).to eql(7_257_600)
+    end
+
+    it 'executes and return the result when an array or a range range is given with proc' do
+      expect(test_arr.my_inject(&test_search)).to eql(test_arr.my_inject(&test_search))
     end
   end
 end
